45accc7b17011604a897cf3480df33cbc86a0233
Samia <68406432+samia-gits-it@users.noreply.github.com>
2022-11-14 15:23:17 -0600

    Update db.js

--- db/db.js
+++ db/db.js
@@ -73,56 +73,54 @@ export default {
 	projects: {
 		web: [
 			{
-				projectName: "Programming Diaries",
+				projectName: "PokeDex",
 				image: "images/programmingdiaries.png",
 				summary:
-					"Developed a full stack blog application to provide content on techical topics across the internet with admin interface.",
-				preview: "https://programmingdiaries.herokuapp.com/",
+					"Developed a Pokedex from scratch fetched from API.",
 				techStack: [
-					"Django",
-					"SQLite",
-					"Bootstrap",
 					"JavaScript",
-					"Heroku",
+					"HTML",
+					"CSS",
+					"jQuery",
+					"Bootstrap",
 				],
 			},
 			{
-				projectName: "Find Your Bank",
+				projectName: "Movie API",
 				image: "images/findyourbank.png",
 				summary:
-					"Developed a React application to render a list of banks fetched from API. Filtered the banks based on queries from localstorage, marked favorites banks.",
-				preview: "https://clever-fermi-0d5d76.netlify.app",
-				techStack: ["Reactjs", "Bootstrap", "JavaScript", "Netlify"],
+					"Developed a movie API.",
+				techStack: ["Reactjs", "Bootstrap", "JavaScript", "Heroku"],
 			},
 			{
 				projectName: "Web Portfolio",
 				image: "images/portfolio.png",
 				summary:
-					"Web Portfolio to showcase acadmics, skills, projects and contact details in better manner.",
+					"As another way to showcase my skill, this very portfolio is another project.",
 				preview: "https://github.com/vinaysomawat/Travographer-Portal",
 				techStack: ["HTML", "Bootstrap", "JavaScript"],
 			},
 			{
-				projectName: "Resume Builder",
+				projectName: "To Do App",
 				image: "images/resume-builder.png",
 				summary:
-					"Browser based editor to build and download Resumes in a customizable templates.",
+					"A simple to do app.",
 				preview: "https://vinaysomawat.github.io/Resume-Builder",
 				techStack: ["HTML", "Bootstrap", "JavaScript"],
 			},
 		],
 		software: [
 			{
-				projectName: "Pizza Ordering ChatBot",
+				projectName: "Nice Guys",
 				image: "images/pizzaorderchatbot.png",
 				summary:
-					"ChatBot using Dialogflow, Firebase database which stores the chat data in the realtime database.",
+					"Forum app for users to protect themselves from predoators.",
 				preview:
 					"https://github.com/vinaysomawat/Pizza-Ordering-ChatBot",
-				techStack: ["Dailogflow", "Firebase"],
+				techStack: ["Backend", "Frontend"],
 			},
 			{
-				projectName: "WhatsApp-Bot",
+				projectName: "Discord-Bot",
 				image: "images/whatsappbot.jpg",
 				summary:
 					"Python script which helps to send messages to WhatsApp contacts automatically using selenium and web automation.",
@@ -178,25 +176,25 @@ export default {
 	},
 	experience: [
 		{
-			title: "Biofourmis India Pvt. Ltd.",
-			duration: "April 2022 - Present",
-			subtitle: "Software Engineer",
+			title: "Selenium Internship Training.",
+			duration: "July 2021 - March 2022",
+			subtitle: "QA Testing Intern",
 			details: [
-				"Working on the products in the healthcare/digital therapeutics domain."
+				"Trained in QA Engineering using Selenium WebDriver, Java, TestNG, and Maven."
 			],
 			tags: [
-				"JavaScript",
-				"Angular",
-				"Bootstrap",
-				"Nodejs",
-				"Jenkins",
+				"Java",
+				"Maven",
+				"Selenium",
+				"TestNG",
+				"Postman",
 			],
 			icon: "heartbeat",
 		},
 		{
-			title: "Novopay Solutions Pvt. Ltd.",
-			duration: "June 2020 - April 2022",
-			subtitle: "Software Engineer",
+			title: "Digital Punk Studios",
+			duration: "TBA",
+			subtitle: "Junior App Developer",
 			details: [
 				"Implemented Aadhaar Enabled Payment services such as Bio-metric eKYC, Cash Withdrawal, Balance Enquiry, Mini-Statements, and Money transfer; completed more than 20 story points in each sprint.",
 				"Integrated QR Code and reduced the effective time by 50 percent to load money into wallet, Clevertap events to track user actions, Freshdesk ticketing service and chat-bot services. Worked on user onboarding, approval, and finance interfaces.",
@@ -213,7 +211,7 @@ export default {
 			icon: "qrcode",
 		},
 		{
-			title: "ThinkPedia LLP",
+			title: "Global Tell Link",
 			duration: "May 2019 - June 2019",
 			subtitle: "SDE Intern",
 			details: [
@@ -228,18 +226,18 @@ export default {
 		{
 			title: "Bachelors in Computer Science and Engineering",
 			duration: "",
-			subtitle: "National Institute of Technology, Warangal",
+			subtitle: "Saint Leo University WorldWide",
 			details: [
-				"Qualified GATE-2020.",
-				"Active Competitive Programmer with CodeChef Rating 1841*.",
-				"Received 500+ stars and 300+ forks on GitHub projects.",
+				"Dean Lister in Fall 2020",
+				"Omega Nu Lambda and other Honors.",
+				"Dived into various programming languages such as C, Java, and C++.",
 			],
 			tags: [
 				"Data Structures & Algorithms",
 				"Operating Systems",
-				"Database Management System",
-				"Computer Networks",
-				"Compiler Designing",
+				"Software Engineering",
+				"Java",
+				"SDLC",
 				"Cloud Computing",
 			],
 			icon: "graduation-cap",
@@ -296,23 +294,13 @@ export default {
 			data: [
 				{
 					text: "Linkedin",
-					link: "https://www.linkedin.com/in/vinaysomawat/",
+					link: "https://www.linkedin.com/in/samiamtn/",
 				},
+				
 				{
-					text: "Twitter",
-					link: "https://twitter.com/thesigmakid",
+					text: "GitHub",
+					link: "https://www.samia-gits-it.github.com/",
 				},
-				{
-					text: "Buy me a coffee",
-					link: "https://www.buymeacoffee.com/r194dME8y",
-				},
-			],
-		},
-		{
-			label: "copyright-text",
-			data: [
-				"Made with &hearts; by Vinay Somawat.",
-				"&copy; No Copyrights. Feel free to use this template."
 			],
 		},
 	],
